fcbcac53958d14a8101e374f27bf2cef69a79b89
nimchimpski <alexwebbuk@gmail.com>
2025-06-09 23:33:28 +0200

    paths to off cloud

--- scripts/run_process/run_process_TSX.py
+++ scripts/run_process/run_process_TSX.py
@@ -17,7 +17,7 @@ from scripts.process.process_helpers import  print_tiff_info_TSX, write_minmax_t
 start=time.time()
 
 logging.basicConfig(
-    level=logging.DEBUG,                            # minimum level to emit
+    level=logging.DEBUG,                            # DEBUG, INFO,[ WARNING,] ERROR, CRITICAL
     format="%(asctime)s %(levelname)-8s %(name)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
